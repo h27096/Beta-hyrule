94c5e936707979182a50483b27f5739b55d33a44
h27096 <h27096@gmail.com>
2026-03-06 18:02:28 -0600

    Add touch controls for movement and actions

--- engine/controls.js
+++ engine/controls.js
@@ -1,1 +1,29 @@
+const keys = {};
 
+function bindButton(id,key){
+
+const btn = document.getElementById(id);
+
+btn.addEventListener("touchstart",e=>{
+e.preventDefault();
+keys[key]=true;
+});
+
+btn.addEventListener("touchend",()=>{
+keys[key]=false;
+});
+
+}
+
+bindButton("up","ArrowUp");
+bindButton("down","ArrowDown");
+bindButton("left","ArrowLeft");
+bindButton("right","ArrowRight");
+
+document.getElementById("attack").addEventListener("touchstart",()=>{
+keys["attack"]=true;
+});
+
+document.getElementById("shield").addEventListener("touchstart",()=>{
+keys["shield"]=true;
+});
