5def6e50afdf20ef35dd5ba713373b1c2dc0bbc1
h27096 <h27096@gmail.com>
2026-03-06 18:03:31 -0600

    Add drawMap function to render a grid map

--- engine/map.js
+++ engine/map.js
@@ -1,1 +1,18 @@
+const mapWidth=20;
+const mapHeight=15;
 
+function drawMap(ctx){
+
+ctx.fillStyle="#2e8b57";
+
+for(let y=0;y<mapHeight;y++){
+
+for(let x=0;x<mapWidth;x++){
+
+ctx.fillRect(x*32,y*32,32,32);
+
+}
+
+}
+
+}
