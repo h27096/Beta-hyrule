8e9cdaa58fa7187eab6ba233f96e2ed81ac554f6
h27096 <h27096@gmail.com>
2026-03-06 18:06:40 -0600

    Refactor player object and movement logic

--- engine/player.js
+++ engine/player.js
@@ -1,28 +1,50 @@
-const player={
-x:9,
-y:6,
-health:5,
-arrows:10
+const player = {
+  x: 9,
+  y: 6,
+  health: 5,
+  arrows: 10,
+  dir: 0
 };
+
+const linkSprite = new Image();
+linkSprite.src = "assets/sprites/link.png";
 
 function movePlayer(){
 
-if(keys["ArrowUp"])player.y--;
-if(keys["ArrowDown"])player.y++;
-if(keys["ArrowLeft"])player.x--;
-if(keys["ArrowRight"])player.x++;
+  if(keys["ArrowUp"]){
+    player.y--;
+    player.dir = 3;
+  }
+
+  if(keys["ArrowDown"]){
+    player.y++;
+    player.dir = 0;
+  }
+
+  if(keys["ArrowLeft"]){
+    player.x--;
+    player.dir = 1;
+  }
+
+  if(keys["ArrowRight"]){
+    player.x++;
+    player.dir = 2;
+  }
 
 }
 
 function drawPlayer(ctx){
 
-ctx.fillStyle="green";
-
-ctx.fillRect(
-player.x*32,
-player.y*32,
-32,
-32
-);
+  ctx.drawImage(
+    linkSprite,
+    0,
+    player.dir * 32,
+    32,
+    32,
+    player.x * 32,
+    player.y * 32,
+    32,
+    32
+  );
 
 }
