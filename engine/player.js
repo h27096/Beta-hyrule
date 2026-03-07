64c293a2160fe8d13f34552947bfd22ddc644633
h27096 <h27096@gmail.com>
2026-03-06 18:03:08 -0600

    Add player object and movement functions

--- engine/player.js
+++ engine/player.js
@@ -1,1 +1,28 @@
+const player={
+x:9,
+y:6,
+health:5,
+arrows:10
+};
 
+function movePlayer(){
+
+if(keys["ArrowUp"])player.y--;
+if(keys["ArrowDown"])player.y++;
+if(keys["ArrowLeft"])player.x--;
+if(keys["ArrowRight"])player.x++;
+
+}
+
+function drawPlayer(ctx){
+
+ctx.fillStyle="green";
+
+ctx.fillRect(
+player.x*32,
+player.y*32,
+32,
+32
+);
+
+}
